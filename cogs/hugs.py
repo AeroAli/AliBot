0cc5fe78b1070a1e1a231e8e9388e8a7b758786d
AeroAli <116851070+AeroAli@users.noreply.github.com>
2022-10-29 01:42:03 +0100

    Add files via upload

---
+++ cogs/hugs.py
@@ -0,0 +1,20 @@
+import random
+import discord
+from discord.ext import commands
+
+
+class Hug(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.hybrid_command()
+    @commands.cooldown(1, 10, commands.BucketType.user)
+    async def hug(self, ctx, user: discord.User):
+        hug_choice = random.choice(open("hugs").read().splitlines())
+        message = f"{ctx.author.mention} hugs <@{user.id}>"
+        await ctx.reply(message)
+        await ctx.reply(content=hug_choice)
+
+
+async def setup(client):
+    await client.add_cog(Hug(client))
---
+++ cogs/quotes.py
@@ -0,0 +1,28 @@
+import random
+from os import listdir
+from os.path import isfile, join
+
+from discord.ext import commands
+
+
+class Quote(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.hybrid_command()
+    @commands.cooldown(1, 10, commands.BucketType.user)
+    async def quote(self, ctx, source: str):
+        try:
+            if source != "random":
+                lines = open(f"quotes/{source}").read().splitlines()
+                quote = random.choice(lines)
+            else:
+                quote = random.choice([x for x in open(
+                    f"quotes/{random.choice([y for y in listdir('quotes') if isfile(join('quotes', y))])}").read().splitlines()])
+            await ctx.reply(quote)
+        except Exception:
+            await ctx.reply("Source Not Available")
+
+
+async def setup(client):
+    await client.add_cog(Quote(client))

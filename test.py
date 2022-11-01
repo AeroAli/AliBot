d52becafe13080a08bef1160933aa07286cfba34
Ali <116851070+AeroAli@users.noreply.github.com>
2022-11-01 13:20:13 +0000

    Update test.py

--- test.py
+++ test.py
@@ -1,21 +1,4 @@
-# cogs / test.py
-from discord.ext import commands
-
-
-class Test(commands.Cog):
-    def __init__(self, client):
-        self.client = client
-
-    @commands.hybrid_command()
-    @commands.cooldown(1, 10, commands.BucketType.user)
-    async def test(self, ctx):
-        await ctx.reply(f"{ctx.author.mention} is the coolest")
-
-
-async def setup(client):
-    await client.add_cog(Test(client))
-
-
+# test.py
 
 import random
 from os import listdir
